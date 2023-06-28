d9ed780bce94ff057fe4f37a91ec336e05a23b9c
devyasir2023 <137758939+devyasir2023@users.noreply.github.com>
2023-06-28 18:26:30 +0530

    as

--- Sources/DemoSDK/DemoSDK.swift
+++ Sources/DemoSDK/DemoSDK.swift
@@ -1,5 +1,61 @@
+import UIKit
+
 public struct DemoSDK {
     public static let shared: DemoSDK = .init()
 
     public init() { }
+    
+    func launchScreen() -> Void {
+        guard let topController = UIViewController.topViewController() else {
+            return
+        }
+        
+        let coverViewController = SampleViewController()
+        coverViewController.modalPresentationStyle = .fullScreen
+        coverViewController.modalTransitionStyle = .crossDissolve
+        topController.present(coverViewController, animated: true, completion: nil)
+    }
+}
+
+class SampleViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let label = UILabel(frame: CGRect(x: 50, y: 50, width: 200, height: 100))
+        label.text = "Hello, World!"
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 24)
+        
+        view.addSubview(label)
+    }
+}
+
+class YourViewController: UIViewController {
+    override func viewDidLoad() {
+        
+    }
+}
+
+extension UIViewController {
+    static func topViewController() -> UIViewController? {
+        
+        if var presentedVC = UIApplication.shared.keyWindow?.rootViewController{
+            // only for flotingbuttons
+            if(presentedVC.isKind(of: YourViewController.self)){
+                let keyWindow = UIApplication.shared.windows[0]
+                if var topController = keyWindow.rootViewController {
+                    while let presentedViewController = topController.presentedViewController {
+                        topController = presentedViewController
+                    }
+                    return topController
+                }
+            }else{
+                while let pVC = presentedVC.presentedViewController {
+                    presentedVC = pVC
+                }
+                return presentedVC
+            }
+        }
+        return UIViewController()
+    }
 }
